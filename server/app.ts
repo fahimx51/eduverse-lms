e7d1720838b50dd6aee0ca6fa414855feb0728e8
fahimx51 <fahim69.dev@gmail.com>
2026-05-19 16:31:32 +0600

    feat: Implement rate limiting using express-rate-limit middleware to enhance API security

--- server/app.ts
+++ server/app.ts
@@ -4,13 +4,22 @@ export const app = express();
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import { ErrorMiddleware } from './middleware/error';
-import ErrorHandler from './utils/ErrorHandler';
 import userRouter from './routes/user.route';
 import courseRouter from './routes/course.route';
 import orderRouter from './routes/order.route';
 import notificationRouter from './routes/notification.route';
 import analyticRouter from './routes/analytics.route';
 import layoutRouter from './routes/layout.route';
+import { rateLimit } from 'express-rate-limit'
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: process.env.NODE_ENV === 'development' ? 999999999 : 100,
+    standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+    ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+    // store: ... , // Redis, Memcached, etc. See below.
+});
 
 //body parser
 app.use(express.json({ limit: '50mb' }));
@@ -43,5 +52,6 @@ app.get("/api/test", (req: Request, res: Response, next: NextFunction) => {
 // unknown route
 
 
+app.use(limiter);
 // error middleware
 app.use(ErrorMiddleware);
